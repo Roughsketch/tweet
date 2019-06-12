8cd8db455daad6dad87b99ff090a1813661fb31b
Maid Dog <maiddogsrl@gmail.com>
2019-06-11 22:42:33 -0500

    Start adding doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -10,43 +10,77 @@ impl FromStr for Tweet {
     }
 }
 
+/// Represents a tweet
 #[derive(Debug, Deserialize)]
 pub struct Tweet {
+    /// When the tweet was posted
     pub created_at: String,
+    /// The unique id for the tweet
     pub id: u64,
+    /// String version of `id`
     pub id_str: String,
+    /// The possibly truncated text of the tweet
     pub text: String,
+    /// The client that posted the tweet
     pub source: String,
+    /// Whether or not the text field is truncated to 140 characters
     pub truncated: bool,
+    /// If this tweet is a reply, this will contain the original tweet id
     pub in_reply_to_status_id: Option<u64>,
+    /// Same as `in_reply_to_status_id`, but a String
     pub in_reply_to_status_id_str: Option<String>,
+    /// If this tweet is a reply, this will contain the original author id
     pub in_reply_to_user_id: Option<u64>,
+    /// Same as `in_reply_to_user_id`, but a String
     pub in_reply_to_user_id_str: Option<String>,
+    /// If this tweet is a reply, contains the original user's screen name
     pub in_reply_to_screen_name: Option<String>,
+    /// The user who posted this tweet
     pub user: User,
+    /// Represents the geographic location of this tweet as reported by user/client
     pub coordinates: Option<Coordinates>,
+    /// The place that this tweet is associated with
     pub place: Option<Place>,
+    /// If this tweet is a quote, it contains the of the quoted tweet id
     pub quoted_status_id: Option<u64>,
+    /// Same as `quoted_status_id_str`, but a String
     pub quoted_status_id_str: Option<String>,
+    /// Whether this is a quoted tweet or not
     pub is_quote_status: bool,
+    /// When the tweet is a quote tweet, this contains the quoted tweet
     pub quoted_status: Option<Box<Tweet>>,
+    /// When the tweet is a retweet, this contains the retweeted tweet
     pub retweeted_status: Option<Box<Tweet>>,
+    /// Approximate count of times tweet was quoted
     pub quote_count: Option<u32>,
+    /// How many times this tweet has been replied to
     pub reply_count: u32,
+    /// How many times this tweet has been retweeted
     pub retweet_count: u32,
+    /// How many times this tweet has been favorited
     pub favorite_count: Option<u32>,
+    /// Entities that have been parsed from the tweet
     pub entities: Option<Entity>,
+    /// If there are media entities, this field contains them all
     pub extended_entities: Option<ExtendedEntity>,
+    /// Whether the authenticated user favorited this tweet
     pub favorited: Option<bool>,
+    /// Whether the authenticated user retweeted this tweet
     pub retweeted: bool,
+    /// Whether a link in this tweet (including media) is potentially sensitive
     pub possibly_sensitive: Option<bool>,
+    /// What filter level is associated with this tweet. Can be none, low, or medium.
     pub filter_level: String,
+    /// BCP 47 language identifier corresponding to machine-detected language of tweet
     pub lang: Option<String>,
     //  matching_rules: Vec<Rule>,
     //  current_user_retweet: 
     //  scopes
+    /// Indicates whether content was removed via DMCA
     pub withheld_copyright: Option<bool>,
+    /// Indicates what countries this tweet is unavailable
     pub withheld_in_countries: Option<Vec<String>>,
+    /// Indicates whether content is being withheld because of "status" or "user"
     pub withheld_scope: Option<String>,
 }
 
@@ -131,73 +165,152 @@ impl Tweet {
     }
 }
 
+/// Represents a twitter user
 #[derive(Debug, Deserialize)]
 pub struct User {
+    /// Unique identifier for the user
     pub id: u64,
+    /// Same as `id`, but a String
     pub id_str: String,
+    /// The user's display name
     pub name: String,
+    /// The user's handle
     pub screen_name: String,
+    /// The user-defined location of the user
     pub location: Option<String>,
     //  derived: Vec<Enrichment>,
+    /// User-provided URL associated with their profile
     pub url: Option<String>,
+    /// User-provided description of their profile
     pub description: Option<String>,
+    /// Whether this user's tweets are protected or not
     pub protected: bool,
+    /// Whether this user is verified or not
     pub verified: bool,
+    /// How many followers this user has
     pub followers_count: u32,
+    /// How many friends this user has
     pub friends_count: u32,
+    /// How many public lists this user is a member of
     pub listed_count: u32,
+    /// How many favorites this user has
     pub favourites_count: u32,
+    /// How many tweets and retweets this user has
     pub statuses_count: u32,
+    /// When the account was created
     pub created_at: String,
+    /// User's uploaded profile banner
     pub profile_banner_url: Option<String>,
+    /// User's uploaded profile image
     pub profile_image_url_https: String,
+    /// When true, indicates user has not changed theme or background of their profile
     pub default_profile: bool,
+    /// Whether or not the user has a default profile image or not
     pub default_profile_image: bool,
+    /// Which countries this user is blocked in
     pub withheld_in_countries: Option<Vec<String>>,
+    /// Indicates whether content being withheld is a "user"
     pub withheld_scope: Option<String>,
 }
 
+/// Represents geographic coordinates
 #[derive(Debug, Deserialize)]
 pub struct Coordinates {
-    pub coordinates: Vec<f64>,
+    /// Latitude and Longitude
+    pub coordinates: (f64, f64),
+    /// Type of data encoded in coordinates
     #[serde(rename = "type")]
     pub kind: String,
 }
 
+/// Represents a specific named location corresponding with geo coordinates
 #[derive(Debug, Deserialize)]
 pub struct Place {
+    /// Unique id representing this place
     pub id: String,
+    /// URL representing location of additional place metadata
     pub url: String,
+    /// Type of location represented by this place
     pub place_type: String,
+    /// Short, human-readable place name
     pub name: String,
+    /// Full, human-readable place name
     pub full_name: String,
+    /// Country code that this place is located in
     pub country_code: String,
+    /// Country that this place is located in
     pub country: String,
+    /// A bounding box containing the coordinates which enclose this place
     pub bounding_box: BoundingBox,
     //  attributes: No docs?
 }
 
+/// Represents a bounding box of geo coordinates
 #[derive(Debug, Deserialize)]
 pub struct BoundingBox {
-    pub coordinates: Vec<Vec<Vec<f64>>>,
+    pub coordinates: Vec<Vec<(f64, f64)>>,
 }
 
+/// Contains information on various parsed out pieces of tweets
 #[derive(Debug, Deserialize)]
 pub struct Entity {
+    /// Collection of hashtags that were included in this tweet
     pub hashtags: Vec<Hashtag>,
+    /// List of links that were included in this tweet
     pub urls: Vec<Url>,
+    /// List of users mentioned in this tweet
     pub user_mentions: Vec<UserMention>,
+    /// List of symbols in this tweet
     pub symbols: Vec<Symbol>,
+    /// List of any media added to this tweet
     pub media: Option<Vec<Media>>,
+    /// List of any polls added to this tweet
     pub polls: Option<Vec<Poll>>,
 }
 
+/// Represents a hashtag
 #[derive(Debug, Deserialize)]
 pub struct Hashtag {
+    /// The indices of the hastag in the original tweet message
+    pub indices: Vec<u32>,
+    /// The text of the hashtag
+    pub text: String,
+}
+
+/// Represents a link from a tweet
+#[derive(Debug, Deserialize)]
+pub struct Url {
+    pub display_url: String,
+    pub expanded_url: String,
+    pub indices: Vec<u32>,
+    pub url: String,
+    pub unwound: Option<UnwoundUrl>
+}
+
+#[derive(Debug, Deserialize)]
+pub struct UnwoundUrl {
+    pub url: String,
+    pub status: u32,
+    pub title: String,
+    pub description: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct UserMention {
+    pub id: Option<u64>,
+    pub id_str: Option<String>,
+    pub indices: Vec<u32>,
+    pub name: Option<String>,
+    pub screen_name: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Symbol {
     pub indices: Vec<u32>,
     pub text: String,
 }
 
+///
 #[derive(Debug, Deserialize)]
 pub struct Media {
     pub display_url: String,
@@ -252,38 +365,6 @@ pub struct Size {
     pub w: u32,
     pub h: u32,
     pub resize: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Url {
-    pub display_url: String,
-    pub expanded_url: String,
-    pub indices: Vec<u32>,
-    pub url: String,
-    pub unwound: Option<UnwoundUrl>
-}
-
-#[derive(Debug, Deserialize)]
-pub struct UnwoundUrl {
-    pub url: String,
-    pub status: u32,
-    pub title: String,
-    pub description: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct UserMention {
-    pub id: Option<u64>,
-    pub id_str: Option<String>,
-    pub indices: Vec<u32>,
-    pub name: Option<String>,
-    pub screen_name: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Symbol {
-    pub indices: Vec<u32>,
-    pub text: String,
 }
 
 #[derive(Debug, Deserialize)]
