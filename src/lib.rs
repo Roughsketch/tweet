f9c9bd2fc300947de623ae761bbe25d38ccc69f5
Maid Dog <maiddogsrl@gmail.com>
2019-06-15 13:36:08 -0500

    Add TwitterResponse enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
+use serde_derive::{Deserialize, Serialize};
+use std::str::FromStr;
+
 mod model;
 mod util;
 pub use crate::model::*;
 
+impl FromStr for TwitterResponse {
+    type Err = serde_json::error::Error;
 
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        serde_json::from_str(s)
+    }
+}
 
-
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum TwitterResponse {
+    Tweet(Tweet),
+    Limit(Limit),
+}
